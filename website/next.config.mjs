b2658d33d0a2d1404bd26d95a38303cec10fe6d3
The007Programmer <aahils2019@gmail.com>
2025-11-13 19:05:42 -0600

    SAFE: update nextconfig shi

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -2,11 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   // Skip Node.js version check when using Bun
-  experimental: {
-    skipNodeCheck: true,
-    // Enable Bun for minification and other tasks
-    useBun: true
-  }
 };
 
 export default nextConfig;
